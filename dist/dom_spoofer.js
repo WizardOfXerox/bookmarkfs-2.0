2eae05ec14e0952af78c60936be8fd80650be1f1
WizardOfXerox <wizardofxerox@users.noreply.github.com>
2026-08-11 09:02:54 +0800

    feat: expand DOM spoofer with hardware concurrency, RAM memory, and screen resolution overrides

--- dist/dom_spoofer.js
+++ dist/dom_spoofer.js
@@ -24,32 +24,67 @@
             let platform = "Win32";
             let vendor = "Google Inc.";
             let isMobile = false;
+            let cores = 8;
+            let memory = 8;
+            let screenWidth = 1920;
+            let screenHeight = 1080;
+            let pixelRatio = 1;
 
             if (ua.includes("Macintosh") || ua.includes("Mac OS X")) {
                 platform = "MacIntel";
                 vendor = (ua.includes("Safari") && !ua.includes("Chrome")) ? "Apple Computer, Inc." : "Google Inc.";
+                cores = 10;
+                memory = 16;
+                screenWidth = 2560;
+                screenHeight = 1440;
+                pixelRatio = 2;
             } else if (ua.includes("Linux") && !ua.includes("Android")) {
                 platform = "Linux x86_64";
                 vendor = ua.includes("Firefox") ? "" : "Google Inc.";
+                cores = 12;
+                memory = 16;
+                screenWidth = 1920;
+                screenHeight = 1080;
             } else if (ua.includes("Android")) {
                 platform = "Linux armv8l";
                 vendor = "Google Inc.";
                 isMobile = true;
+                cores = 8;
+                memory = 6;
+                screenWidth = 412;
+                screenHeight = 915;
+                pixelRatio = 3;
             } else if (ua.includes("iPhone") || ua.includes("iPad")) {
                 platform = ua.includes("iPad") ? "iPad" : "iPhone";
                 vendor = "Apple Computer, Inc.";
                 isMobile = true;
+                cores = 6;
+                memory = 6;
+                screenWidth = 393;
+                screenHeight = 852;
+                pixelRatio = 3;
+            } else if (ua.includes("PlayStation") || ua.includes("Xbox") || ua.includes("Nintendo")) {
+                platform = ua.includes("PlayStation") ? "PlayStation 5" : (ua.includes("Xbox") ? "Xbox One" : "Nintendo Switch");
+                vendor = "";
+                screenWidth = 1920;
+                screenHeight = 1080;
             }
             if (ua.includes("Firefox")) vendor = "";
 
-            // Override navigator properties
+            // Override navigator core properties
             Object.defineProperty(navigator, 'userAgent', { get: () => ua, configurable: true, enumerable: true });
             Object.defineProperty(navigator, 'appVersion', { get: () => ua.replace(/^Mozilla\//, ''), configurable: true, enumerable: true });
             Object.defineProperty(navigator, 'platform', { get: () => platform, configurable: true, enumerable: true });
             Object.defineProperty(navigator, 'vendor', { get: () => vendor, configurable: true, enumerable: true });
-            
+            Object.defineProperty(navigator, 'hardwareConcurrency', { get: () => cores, configurable: true, enumerable: true });
+            if ('deviceMemory' in navigator) {
+                Object.defineProperty(navigator, 'deviceMemory', { get: () => memory, configurable: true, enumerable: true });
+            }
+
             if (isMobile) {
                 Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 5, configurable: true, enumerable: true });
+            } else {
+                Object.defineProperty(navigator, 'maxTouchPoints', { get: () => 0, configurable: true, enumerable: true });
             }
 
             // Language override
@@ -58,6 +93,17 @@
                 const langs = settings.acceptLanguage.split(",").map(l => l.split(";")[0].trim()).filter(Boolean);
                 Object.defineProperty(navigator, 'language', { get: () => lang, configurable: true, enumerable: true });
                 Object.defineProperty(navigator, 'languages', { get: () => langs, configurable: true, enumerable: true });
+            }
+
+            // Screen property overrides
+            if (isMobile || settings.domSpoofScreen) {
+                try {
+                    Object.defineProperty(window.screen, 'width', { get: () => screenWidth, configurable: true, enumerable: true });
+                    Object.defineProperty(window.screen, 'height', { get: () => screenHeight, configurable: true, enumerable: true });
+                    Object.defineProperty(window.screen, 'availWidth', { get: () => screenWidth, configurable: true, enumerable: true });
+                    Object.defineProperty(window.screen, 'availHeight', { get: () => screenHeight - 40, configurable: true, enumerable: true });
+                    Object.defineProperty(window, 'devicePixelRatio', { get: () => pixelRatio, configurable: true, enumerable: true });
+                } catch (scErr) {}
             }
 
             // Client Hints (userAgentData)
